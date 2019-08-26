75c8db1738f4d41bcfc405216330394eb0ba62b8
GennadySX <htcucell@gmail.com>
2019-08-27 00:50:34 +0300

    ps -start :Finish

---
+++ PrettyUrl.php
@@ -0,0 +1,53 @@
+<?php 
+
+
+
+class PrettyUrl {
+
+
+
+   public function __constructor($stringCyrillic) {
+        return $this->prettyUrl($stringCyrillic);
+    }
+
+ /*
+     * translating cyrillic to latin with - exponse for item url
+     * Created by GennadySX at 2019.08.27
+     * @param string cyrillic words
+     * @return latin url address
+     * this function might be static for calling PrettyUrl::prettyUrl($yourCryllicLink);
+     * Don't forget set your namespace app location
+     * Be happy ;) 
+     */
+    public function prettyUrl($stringCyrillic)
+    {
+        return trim(preg_replace('~[^-a-z0-9_]+~u', '-', strtolower(strtr($stringCyrillic,
+            array(
+                'а' => 'a', 'б' => 'b', 'в' => 'v',
+                'г' => 'g', 'д' => 'd', 'е' => 'e',
+                'ё' => 'e', 'ж' => 'zh', 'з' => 'z',
+                'и' => 'i', 'й' => 'y', 'к' => 'k',
+                'л' => 'l', 'м' => 'm', 'н' => 'n',
+                'о' => 'o', 'п' => 'p', 'р' => 'r',
+                'с' => 's', 'т' => 't', 'у' => 'u',
+                'ф' => 'f', 'х' => 'h', 'ц' => 'c',
+                'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch',
+                'ь' => '\'', 'ы' => 'y', 'ъ' => '\'',
+                'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
+
+                'А' => 'A', 'Б' => 'B', 'В' => 'V',
+                'Г' => 'G', 'Д' => 'D', 'Е' => 'E',
+                'Ё' => 'E', 'Ж' => 'Zh', 'З' => 'Z',
+                'И' => 'I', 'Й' => 'Y', 'К' => 'K',
+                'Л' => 'L', 'М' => 'M', 'Н' => 'N',
+                'О' => 'O', 'П' => 'P', 'Р' => 'R',
+                'С' => 'S', 'Т' => 'T', 'У' => 'U',
+                'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C',
+                'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sch',
+                'Ь' => '\'', 'Ы' => 'Y', 'Ъ' => '\'',
+                'Э' => 'E', 'Ю' => 'Yu', 'Я' => 'Ya',
+            )))), "-");
+    }
+
+
+}
